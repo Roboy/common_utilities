c179909ac5c32f342c6f5e04ead0b8a49d84b087
letrend <simon.trendel@tum.de>
2016-12-14 13:54:55 +0100

    new version of urdf, fixed this

--- bash/makeSDFfromURDFexport.bash
+++ bash/makeSDFfromURDFexport.bash
@@ -4,14 +4,14 @@ if [ "$#" -ne 1 ]; then
 else
     currentworkingdirectory=$(pwd)
     cd $1
-    projectname=$(ls robots|sed -e "s/.URDF//")
+    projectname=$(ls urdf|sed -e "s/.urdf//")
     mkdir $projectname
     cd $projectname
     mkdir cad
     cp ../meshes/* cad
     #python $currentworkingdirectory/../python/step-stl-dae/stl-to-dae.py cad/ dae/
-    gz sdf -p ../robots/$projectname.URDF > model.sdf
-    #grep -rl "STL" model.sdf | xargs sed -i "s/STL/dae/g"
+    gz sdf -p ../urdf/$projectname.urdf > model.sdf
+    grep -rl "stl" model.sdf | xargs sed -i "s/stl/STL/g"
     grep -rl "meshes" model.sdf | xargs sed -i "s/meshes/cad/g"
     cp $currentworkingdirectory/model.config .
     grep -rl "PROJECTNAME" model.config | xargs sed -i "s/PROJECTNAME/$projectname/g"
