08e663bbaa38229d14a2d3b71f22aebadcaa081e
letrend <simon.trendel@tum.de>
2017-05-09 16:21:17 +0200

    with messages for powerlink

--- include/common_utilities/CommonDefinitions.h
+++ include/common_utilities/CommonDefinitions.h
@@ -1,29 +1,47 @@
-//
-// Created by bruh on 1/8/16.
-//
+/** CommonDefinitions.h
+ *  Overall defines that are used by all nodes
+ */
 
-#ifndef ROBOY_CONTROL_COMMONDEFINITIONS_H
-#define ROBOY_CONTROL_COMMONDEFINITIONS_H
+#pragma once
 
-enum ControlMode {
-		UNDEFINED_CONTROL = 0,
-		POSITION_CONTROL,
-		VELOCITY_CONTROL,
-		FORCE_CONTROL,
-		MUSCLE_ACTIVITY_CONTROL
+#include <stdint.h>
+#define NUMBER_OF_CONTROL_MODES 3
+#define NUMBER_OF_MOTORS_PER_FPGA 14
+
+enum ControlMode{
+	POSITION = 0,
+	VELOCITY,
+	DISPLACEMENT,
+	FORCE
 };
 
 static const char * controlModeStrings[] = {
-		"undefined",
 		"position",
 		"velocity",
-		"force",
-		"muscle_activity"
+		"displacement",
+		"force"
 };
 
 static const char * getControlModeString(ControlMode mode) {
 		return controlModeStrings[mode];
 }
+
+typedef struct
+{
+    uint8_t control_mode;
+    int32_t outputPosMax; /*!< maximum control output in the positive direction in counts, max 4000*/
+    int32_t outputNegMax; /*!< maximum control output in the negative direction in counts, max -4000*/
+    int32_t spPosMax;/*<!Positive limit for the set point.*/
+    int32_t spNegMax;/*<!Negative limit for the set point.*/
+    uint16_t Kp;/*!<Gain of the proportional component*/
+    uint16_t Ki;/*!<Gain of the integral component*/
+    uint16_t Kd;/*!<Gain of the differential component*/
+    uint16_t forwardGain; /*!<Gain of  the feed-forward term*/
+    uint16_t deadBand;/*!<Optional deadband threshold for the control response*/
+    int16_t IntegralPosMax; /*!<Integral positive component maximum*/
+    int16_t IntegralNegMax; /*!<Integral negative component maximum*/
+    float radPerEncoderCount = {2 * 3.14159265359f / (2000.0f * 53.0f)};
+}control_Parameters_t;
 
 enum ControllerState {
 		UNDEFINED = 0,
@@ -89,11 +107,6 @@ enum SIMULATIONCONTROL{
 		StopRecording
 };
 
-enum{
-    POSITION = 0,
-    VELOCITY
-};
-
 enum PLANE{
     TRAVERSAL,
     SAGITTAL,
@@ -104,5 +117,3 @@ enum PLANE{
 #define degreesToRadians(angleDegrees) (angleDegrees * (float)M_PI / 180.0f)
 // Converts radians to degrees.
 #define radiansToDegrees(angleRadians) (angleRadians * 180.0f / (float)M_PI)
-
-#endif //ROBOY_CONTROL_COMMONDEFINITIONS_H
