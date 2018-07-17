62219b87f96ba419cddca31eee51a0842e439eeb
letrend <simon.trendel@tum.de>
2018-07-17 04:01:51 +0200

    added ignore for smaller parts

--- python/reduceMeshesInDirectory.py
+++ python/reduceMeshesInDirectory.py
@@ -1,5 +1,6 @@
 from subprocess import call
 import sys
+import os
 from os import listdir
 from os.path import isfile, join
 
@@ -7,19 +8,23 @@ argv = sys.argv
 
 print(argv)
 
-if len(argv)<=4 and len(argv)<6:
+if len(argv) <= 4 and len(argv) < 6:
     print('USAGE: path/to/meshes/directory/ outputpath/directory/ ration[0-1] OPTIONAL: scale[0.01-1000]')
 else:
-    files = [f for f in listdir(argv[1]) ]
-    print('Converting '+ str(len(files)) +' files')
-    i=0
+    files = [f for f in listdir(argv[1])]
+    print('Converting ' + str(len(files)) + ' files')
+    i = 0
     scale = 1
-    if(len(argv)==5):
-	scale = argv[4]
+    if (len(argv) == 5):
+        scale = argv[4]
     for file in files:
-        if file.endswith('.stl') or file.endswith('.STL') :
-            i+=1
+        if (file.endswith('.stl') or file.endswith('.STL')) and os.path.getsize(join(argv[1], file)) > 100000:  # for files bigger that 100kB
+            i += 1
             print('Converting ( ' + str(i) + '/' + str(len(files)) + ' )')
-            call(["blender", "--background", "--python", "reduceMesh.py", "--", join(argv[1],file), join(argv[2],file), argv[3], scale])
-    if(i<len(files)):
-	print('there where some none stl files, didn\'t touch them though');
+            call(
+                ["blender", "--background", "--python", "reduceMesh.py", "--", join(argv[1], file), join(argv[2], file),
+                 argv[3], scale])
+        else:
+            print('ignoring ' + join(argv[1], file))
+    if (i < len(files)):
+        print('there where some files that were either no stls or too small (<100kB), didn\'t touch them though');
